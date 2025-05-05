b0e541a40ec3307d743ae5f1c1c112c9b7ae2fb1
Fernando Guerrero <nandofer27_98@outlook.com>
2025-05-05 14:19:28 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// Mi Primera Clase
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe tu nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Mi Nombre es: " + nombre);
+    }
+}
