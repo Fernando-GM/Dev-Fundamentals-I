a9170318e279f04ff1d3be624b5451b674f6be79
Fernando Guerrero <nandofer27_98@outlook.com>
2025-05-09 12:55:16 -0600

    Cambio en clase Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escribe tu nombre");
+        System.out.print("Escribe tu nombre");
         String nombre = scanner.nextLine();
         System.out.println("Mi Nombre es: " + nombre);
     }
