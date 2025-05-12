31a9c0f04d9039be57e4ea4adda9d2524bd6c86e
Fernando Guerrero <nandofer27_98@outlook.com>
2025-05-12 13:56:33 -0600

    Ejemplo de break y continue

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Escribe tu nombre");
-        String nombre = scanner.nextLine();
-        System.out.println("Mi Nombre es: " + nombre);
+//        int n0 = 0, n1 = 1;
+//        int n2 = n0 + n1;
+//        while (n2 < 1000) {
+//            n0 = n1;
+//            n1 = n2;
+//            n2 = n0 + n1;
+//        }
+        for (int i= 0; i< 10; i++) {
+            // Codigo...
+            if (i == 4) {
+//                return; // Fin del programa en public static void main
+//                continue;
+                 break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Fin del programa");
     }
 }
